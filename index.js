5fa1b28a7c53186142a76ff30ed1ed20e3dfdfc0
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-04-30 11:54:44 +0530

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.listen(8800, () => {
+  connect()
   console.log("Connected to backend!");
 });
