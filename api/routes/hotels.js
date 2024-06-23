72c819ea3317df88e03e67e60b619eabf498af55
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-06-23 16:51:04 +0530

    commit

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
@@ -19,12 +21,14 @@ router.put("/:id", verifyAdmin, updateHotel);
 
 // DELETE
 
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.delete("/find/:id", verifyAdmin, deleteHotel);
 // GET
 
 router.get("/:id", getHotel);
 // GET ALL
 
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router;
