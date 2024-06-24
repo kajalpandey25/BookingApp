3b3a3f255473c50156e5b3c17a3f89976d61810f
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-06-24 16:42:33 +0530

    commit

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -44,7 +44,7 @@ export const getHotel = async (req, res, next) => {
 export const getHotels = async (req, res, next) => {
  
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find(req.query).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
